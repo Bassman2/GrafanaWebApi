544ef52ecfaad09d4f0687f3f88efe375688f005
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-08-23 20:52:00 +0200

    Update

--- Src/GrafanaWebApiShare/Model/HealthModel.cs
+++ Src/GrafanaWebApiShare/Model/HealthModel.cs
@@ -1,5 +1,8 @@
-﻿namespace GrafanaWebApi.Model;
+﻿using WebServiceClient.Attributes;
 
+namespace GrafanaWebApi.Model;
+
+[ModelAttribute(value: 5, access: ModelAccess.Internal, name: "Paul", flag: true, Value2 = 32, Access2 = ModelAccess.Protected, Name2 = "Peter", Flag2 = true)]
 internal class HealthModel
 {
     [JsonPropertyName("database")]
